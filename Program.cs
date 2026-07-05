73e3dcf4b86a73758d38ce4e858a68c6e5a70aa1
TheR7angelo <raphael.antunes@hotmail.fr>
2026-07-05 12:13:23 +0200

    refactor(Program.cs): Remove language detection and culture setting code

--- Program.cs
+++ Program.cs
@@ -1,7 +1,5 @@
-using System.Globalization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.JSInterop;
 using TheR7angelo.github.io;
 using MudBlazor.Services;
 
@@ -15,22 +13,5 @@ builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.H
 builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
 
 var host = builder.Build();
-
-var jsRuntime = host.Services.GetRequiredService<IJSRuntime>();
-
-try
-{
-    var browserLang = await jsRuntime.InvokeAsync<string>("window.navigator.language");
-    if (!string.IsNullOrEmpty(browserLang))
-    {
-        var customCulture = new CultureInfo(browserLang);
-        CultureInfo.DefaultThreadCurrentCulture = customCulture;
-        CultureInfo.DefaultThreadCurrentUICulture = customCulture;
-    }
-}
-catch
-{
-    // Ignore
-}
 
 await host.RunAsync();
