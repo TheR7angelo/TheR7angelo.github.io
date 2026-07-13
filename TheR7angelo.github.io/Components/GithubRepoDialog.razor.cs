1d1f7c7d6674ff7feaeb3007f0f0b0b77750826c
TheR7angelo <raphael.antunes@hotmail.fr>
2026-07-13 14:15:33 +0200

    feat: Add GithubRepoDialog component for displaying detailed information about a GitHub repository

---
+++ TheR7angelo.github.io/Components/GithubRepoDialog.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using TheR7angelo.github.io.Service.Models.GitHub;
+
+namespace TheR7angelo.github.io.Components;
+
+public partial class GithubRepoDialog
+{
+    [CascadingParameter]
+    private IMudDialogInstance MudDialog { get; set; } = null!;
+
+    [Parameter]
+    public GithubRepositoryInformationDto Project { get; set; } = null!;
+
+    private void Close()
+        => MudDialog.Close();
+}
